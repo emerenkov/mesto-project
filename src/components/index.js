4bec80e45af2fce3736d83f40da65b19b284d0f4
Evgeniy Marchal <evgeniymarchal@yandex.ru>
2022-03-12 01:31:15 +0300

    promis

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import '../pages/index.css'; // добавьте импорт главного файла стилей
 import {enableValidation, objEnableValidation} from "./validate";
-
-import {itemPopup, editPopup, addButton, profileName, profileCareer, buttonEditProfile,
+import API from './api.js';
+import {createCard, cardElements} from "./card";
+import {itemPopup, editPopup, avatarPopup, avatarButton, profileAvatarButton, addButton, profileName, profileCareer, buttonEditProfile,
   nameInput, jobInput, form} from './modal.js'
 
 const imagePopup = document.querySelector('#image-popup');//
@@ -9,7 +10,26 @@ const imageFigcaption = imagePopup.querySelector('.popup__image-figcaption');//
 const photoPopup = imagePopup.querySelector('.popup__image');//
 const imageClosePopup = imagePopup.querySelector('.popup__button-close');//
 
+
+
+
 //----------------------------------ВАЛИДАЦИЯ ФОРМ---------------------------------------------------------------------
+Promise.all([API.profileData(), API.cardData()])
+  .then(([user, cards]) => {
+
+    profileName.textContent = user.name;
+    profileCareer.textContent = user.about;
+    avatarButton.src = user.avatar;
+
+    const newCards = cards.map(function (card) {
+      return createCard(card.name, card.link, card._id, card.likes, user._id, card.owner)
+    });
+    cardElements.prepend(...newCards);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 
 enableValidation(objEnableValidation);
 
@@ -31,3 +51,8 @@ addButton.addEventListener('click', function () {
   form.reset();
   openPopup(itemPopup);
 });
+
+profileAvatarButton.addEventListener('click', function () {
+  openPopup(avatarPopup);
+})
+
